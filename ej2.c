5d8248ef5dfce8e6b4adce19658945764aa6ad62
Ignacio Losiggio <iglosiggio@gmail.com>
2022-12-03 15:48:50 -0300

    Ej2 en constant expressions de C

---
+++ ej2.c
@@ -0,0 +1,10 @@
+#define A + "\3\6\0"
+#define B + "\0\3\6"
+#define C + "\6\0\3"
+#define X [0] + 1
+#define Y [1] + 2
+#define Z [2] + 3
+
+static long resultado = 0 +
+#include "ej2.input"
+;
---
+++ ej2b.c
@@ -0,0 +1,10 @@
+#define A + "\3\4\x8"
+#define B + "\1\5\x9"
+#define C + "\2\6\7"
+#define X [0]
+#define Y [1]
+#define Z [2]
+
+static long resultado = 0 +
+#include "ej2.input"
+;
