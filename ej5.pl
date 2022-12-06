21fd238d9c5dd1dfc7b4d7dcf3feb3470dd8f0a9
Ignacio Losiggio <iglosiggio@gmail.com>
2022-12-05 21:13:05 -0300

    Agrego el ejercicio b

--- ej5.pl
+++ ej5.pl
@@ -24,7 +24,7 @@ commands([command(Amount, From, To)|XS]) -->
     commands(XS).
 commands([]) --> [].
 
-move(Stacks, command(Amount, From, To), NewStacks) :-
+move_ej1(command(Amount, From, To), Stacks, NewStacks) :-
     nth1(From, Stacks, FromStack),
     nth1(To, Stacks, ToStack),
     split_at(Amount, FromStack, Extracted, NewFromStack),
@@ -33,10 +33,13 @@ move(Stacks, command(Amount, From, To), NewStacks) :-
     select(FromStack, Stacks, NewFromStack, ReplacedFromStack),
     select(ToStack, ReplacedFromStack, NewToStack, NewStacks).
 
-run_commands(Stacks, [Command|Commands], FinalStacks) :-
-    move(Stacks, Command, NextStacks),
-    run_commands(NextStacks, Commands, FinalStacks).
-run_commands(Stacks, [], Stacks).
+move_ej2(command(Amount, From, To), Stacks, NewStacks) :-
+    nth1(From, Stacks, FromStack),
+    nth1(To, Stacks, ToStack),
+    split_at(Amount, FromStack, WillAddToStack, NewFromStack),
+    append(WillAddToStack, ToStack, NewToStack),
+    select(FromStack, Stacks, NewFromStack, ReplacedFromStack),
+    select(ToStack, ReplacedFromStack, NewToStack, NewStacks).
 
 % From: https://www.swi-prolog.org/pldoc/doc/_SWI_/library/dialect/hprolog.pl?show=src#split_at/4
 split_at(0,L,[],L) :- !.
@@ -46,8 +49,11 @@ split_at(N,[H|T],[H|L1],L2) :-
 
 head([X|_], X).
 
+run_ej(Move, ResultsFormatted) :-
+    phrase_from_file(main_file(Stacks, Commands), "ej5.input"),
+    foldl(Move, Commands, Stacks, NewStacks),
+    maplist(head, NewStacks, Results),
+    concat_atom(Results, ResultsFormatted).
 
-?- phrase_from_file(main_file(Stacks, Commands), "ej5.input"),
-   run_commands(Stacks, Commands, NewStacks),
-   maplist(head, NewStacks, Results),
-   concat_atom(Results, ResultsFormatted).
+:- run_ej(move_ej1, Result), write(Result), nl.
+:- run_ej(move_ej2, Result), write(Result), nl.
